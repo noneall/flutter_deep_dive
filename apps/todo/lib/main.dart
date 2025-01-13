b28ad01f772f5ecc40cb642d2ae4a8210ba69b6a
Anh Nguyen <nathann.dev@gmail.com>
2025-01-13 00:36:58 -0700

    simple todo app

---
+++ apps/todo/lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const TodoList(),
+    );
+  }
+}
+
+class Todo {
+  String title;
+  bool isDone;
+
+  Todo({required this.title, this.isDone = false});
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  TodoListState createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  final List<Todo> _todos = [];
+  final TextEditingController _textController = TextEditingController();
+
+  void _addTodo(String title) {
+    if (title.isEmpty) return;
+
+    setState(() {
+      _todos.add(Todo(title: title));
+    });
+    _textController.clear();
+  }
+
+  void _toggleTodo(int index) {
+    setState(() {
+      _todos[index].isDone = !_todos[index].isDone;
+    });
+  }
+
+  void _removeTodo(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                      hintText: 'Add a new todo',
+                      border: OutlineInputBorder(),
+                    ),
+                    onSubmitted: _addTodo,
+                  ),
+                ),
+                const SizedBox(width: 16.0),
+                FloatingActionButton(
+                  onPressed: () => _addTodo(_textController.text),
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                final todo = _todos[index];
+                return ListTile(
+                  leading: Checkbox(
+                    value: todo.isDone,
+                    onChanged: (bool? value) => _toggleTodo(index),
+                  ),
+                  title: Text(
+                    todo.title,
+                    style: TextStyle(
+                      decoration:
+                          todo.isDone ? TextDecoration.lineThrough : null,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _removeTodo(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+}
--- packages/todo_main/lib/repositories/todo_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:todo_main/model/todo.dart';
-
-abstract class TodoRepository {
-  Future<List<Todo>> getTodos();
-  Future<Todo> getTodoById(String id);
-  Future<Todo> createTodo(String title, String? description);
-  Future<Todo> updateTodo(Todo todo);
-  Future<void> deleteTodo(String id);
-}
